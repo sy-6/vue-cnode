dfc8d0dbe93104915b5ac7554bbcb03ce381dd2a
sy-6 <249859693@qq.com>
2022-10-01 14:15:36 +0800

    good

--- src/views/gjh.vue
+++ src/views/gjh.vue
@@ -3,19 +3,23 @@
     <h1>gjh</h1>
     <button @click="returnHome">返回</button>
 
-    <template>
+
       <el-table :data="tableData" style="width: 100%">
         <el-table-column prop="title" label="标题" width="180">
         </el-table-column>
         <el-table-column prop="author.loginname" label="用户名" width="180">
         </el-table-column>
-        <el-table-column prop="good" label="是否好人" width="180">
-          
+        <el-table-column  label="是否好人" width="180">
+          <template  slot-scope="scope">
+            <span v-if="scope.row.good === true">好人</span>
+            <span v-if="scope.row.good === false">坏人</span>
+
+          </template>
         </el-table-column>
       </el-table>
       ok: 
       <span>{{ok}}</span>
-    </template>
+
     <!-- 标题，字段：title
  用户名，字段：author对象的loginname属性
 是否好人，字段：good，如果为true,显示好人，false显示坏人 -->
@@ -26,6 +30,9 @@
 export default {
   data() {
     return {
+      searchForm:{
+        good:''
+      },
       tableData: [],
       ok: '初始',
     };
@@ -42,6 +49,9 @@ export default {
     returnHome() {
       this.$router.replace("/");
     },
+    search(){
+
+    }
   },
 };
 </script>
